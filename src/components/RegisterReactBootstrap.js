6b4909c4e91b540358b0cd32e4bf4f6b1598299a
Programming Hero <programming.hero1@gmail.com>
2022-10-12 22:24:17 -0500

    create a new user

--- src/components/RegisterReactBootstrap.js
+++ src/components/RegisterReactBootstrap.js
@@ -1,6 +1,10 @@
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import app from '../firebase/firebase.init';
+
+const auth = getAuth(app);
 
 const RegisterReactBootstrap = () => {
 
@@ -9,6 +13,14 @@ const RegisterReactBootstrap = () => {
         const email = event.target.email.value;
         const password = event.target.password.value;
         console.log(email, password);
+        createUserWithEmailAndPassword(auth, email, password)
+        .then( result =>{
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error => {
+            console.error('error', error);
+        })
     }
 
     return (
@@ -17,12 +29,12 @@ const RegisterReactBootstrap = () => {
             <Form onSubmit={handleRegister}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name='email' placeholder="Enter email" />
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name='password' placeholder="Password" />
+                    <Form.Control type="password" name='password' placeholder="Password" required/>
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Register
